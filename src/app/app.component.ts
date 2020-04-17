6ffd95ba2cc072cf27d4ce900559e39c81464c56
webby12333 <webby12333@yahoo.com>
2020-04-16 21:12:04 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ constructor() {
          this.sourceList.push(satellite);
          // console.log(satellite.shouldShowWarning());
         }
-        
+        this.displayList = this.sourceList.slice(0);
    }.bind(this));
 }.bind(this));
          
@@ -54,12 +54,3 @@ search(searchTerm: string): void {
 }
 
 
-// constructor() {
-//   this.sourceList = [
-//      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-//      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-//      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-//      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-//      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-//   ];
-// }
